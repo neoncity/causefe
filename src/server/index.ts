e6acd2e72d50b0f63b531aed7e7c06c4b285d2fd
Horia Coman <horia141@gmail.com>
2017-01-22 20:16:27 +0200

    Add serving of client from server.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,20 +1,43 @@
-import * as express from 'express';
+import * as express from 'express'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as webpack from 'webpack'
+import * as webpackDevMiddleware from 'webpack-dev-middleware'
 
-import * as config from './config'; 
+import { isLocal } from '@neoncity/common-js/env'
+
+import * as config from './config'
+
 
 async function main() {
 
+    const webpackConfig = require('../../webpack.config.js');
     const app = express();
 
-    app.get('/hello', async (_: express.Request, res: express.Response) => {
-        res.write('<!DOCTYPE html><html>Hello</html>');
-        res.end();
-    });
+    if (isLocal(config.ENV)) {
+        const middleware = webpackDevMiddleware(webpack(webpackConfig), {
+	    publicPath: webpackConfig.output.publicPath,
+	    serverSideRender: false
+        });
+
+        app.use(middleware);
+        app.get('/', (_: express.Request, res: express.Response) => {
+            res.write((middleware as any).fileSystem.readFileSync(path.join(process.cwd(), 'out', 'client', 'index.html')));
+            res.end();
+        });
+    } else {
+        const indexFile = fs.readFileSync(path.join(process.cwd(), 'out', 'client', 'index.html'));
+        
+        app.use('/out/client', express.static(path.join(process.cwd(), 'out', 'client')));
+        app.get('/', (_: express.Request, res: express.Response) => {
+            res.write(indexFile);
+            res.end();
+        });
+    }
 
     app.listen(config.PORT, config.ADDRESS, () => {
 	console.log(`Started ... ${config.ADDRESS}:${config.PORT}`);
     });
-
 }
 
 main();
