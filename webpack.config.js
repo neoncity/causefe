7f42cd22ad299127904d652b07a80feb1c4aee9c
Horia Coman <horia141@gmail.com>
2017-01-22 15:36:34 +0200

    Better packing.

--- webpack.config.js
+++ webpack.config.js
@@ -4,57 +4,10 @@ const failPlugin = require('webpack-fail-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
-const commonConfig = {
-    devtool: 'source-map'
-};
-
-const serverConfig = Object.assign({}, commonConfig, {
-    target: 'node',
-    entry: {
-	app: './src/server/index.ts',
-	tests: './tests/index.ts',
-    },
-    output: {
-        path: path.resolve(__dirname, 'out', 'server'),
-        filename: '[name].js'
-    },
-    module: {
-        loaders: [{
-            test: /\.ts$/,
-            include: [
-                path.resolve(__dirname, 'src', 'server'),
-                path.resolve(__dirname, 'tests')
-            ],
-            loader: 'ts',
-            query: {
-                configFileName: 'tsconfig.json',
-                silent: true
-            }
-        }, {
-	    test: /\.(json)$/,
-	    include: [
-		path.resolve(__dirname, 'src', 'server'),
-		path.resolve(__dirname, 'node_modules')
-	    ],
-	    loader: 'json'
-	}],
-    },
-    plugins: [
-        failPlugin,
-    ],
-    resolve: {
-        extensions: ['', '.js', '.ts'],
-        root: [
-            path.resolve(__dirname, 'src', 'server'),
-            path.resolve(__dirname, 'tests')
-        ]
-    }
-});
-
-const clientConfig = Object.assign({}, commonConfig, {
+module.exports = {
     target: 'web',
     entry: {
-	app: './src/client/index.ts'
+	client: './src/client/index.ts'
     },
     output: {
         path: path.resolve(__dirname, 'out', 'client'),
@@ -69,7 +22,7 @@ const clientConfig = Object.assign({}, commonConfig, {
             ],
             loader: 'ts',
             query: {
-                configFileName: 'tsconfig.json',
+                configFileName: 'tsconfig.client.json',
                 silent: true
             }
         }, {
@@ -95,14 +48,13 @@ const clientConfig = Object.assign({}, commonConfig, {
 	    {from: './src/client/static/index.html'},
 	    {from: './src/client/static/favicon.ico'}
 	]),
-	new ExtractTextPlugin('app.css')
+	new ExtractTextPlugin('client.css')
     ],
     resolve: {
         extensions: ['', '.js', '.ts', '.css', '.less'],
         root: [
             path.resolve(__dirname, 'src', 'client')
         ]
-    }
-});
-
-module.exports = [serverConfig, clientConfig];
+    },
+    devtool: 'source-map'
+};
