cfdcc30de528d20d0f3e03f3c0e8e3611043f0dc
Horia Coman <horia141@gmail.com>
2017-03-21 14:14:44 +0200

    Add reloading of the page for admin stuff, when not logged in.

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -178,20 +178,41 @@ class _AppFrame extends React.Component<AppFrameProps, undefined> {
     
     render() {
 	const userIdSection = this.props.user != null ? <p>User: {this.props.user.name}</p> : <p></p>;
-        return (
-	    <div>
-		<div>This is the app frame</div>
-		<div>
-		  <Link to="/">Home</Link>
-		  <Link to="/c/cause-1">Cause 1</Link>
-		  <Link to="/c/cause-2">Cause 2</Link>
-		  <Link to="/admin">Admin</Link>
-		  <Link to="/console">Console</Link>
-                  {userIdSection}
-		</div>
-		{this.props.children}
-            </div>
-        );
+	// Bit of a hack. If there's no user, the global navigation to admin and console is done through a regular <a> tag
+	// which will trigger a page load event. This is not so bad, as the login flow is beefy as it is, but it does add
+	// _some_ extra complexity. Hopefully it will be easy to get rid of in the future.
+	// TODO: make this simpler / work through the single-page setup.
+        if (this.props.user == null) {
+            return (
+                <div>
+                    <div>This is the app frame</div>
+                    <div>
+                        <Link to="/">Home</Link>
+                        <Link to="/c/cause-1">Cause 1</Link>
+                        <Link to="/c/cause-2">Cause 2</Link>
+                        <a href="/admin">Admin</a>
+                        <a href="/console">Console</a>
+                        {userIdSection}
+                    </div>
+                    {this.props.children}
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <div>This is the app frame</div>
+                    <div>
+                        <Link to="/">Home</Link>
+                        <Link to="/c/cause-1">Cause 1</Link>
+                        <Link to="/c/cause-2">Cause 2</Link>
+                        <Link to="/admin">Admin</Link>
+                        <Link to="/console">Console</Link>
+                        {userIdSection}
+                    </div>
+                    {this.props.children}
+                </div>
+            );      
+        }
     }
 }
 
