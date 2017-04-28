596d38f3b98c9f6e82a092fd773c08d919be4705
Horia Coman <horia141@gmail.com>
2017-04-28 19:55:42 +0300

    Add the donation flow. As _limited_ as it is now.

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -398,32 +398,54 @@ interface PublicCauseWidgetProps {
 }
 
 interface PublicCauseWidgetState {
-    shareIsLoading: boolean;
-    shareIsReady: boolean;
-    shareIsFailed: boolean;
+    donationAmount: UserInput<number, number>;
+    donationState: OpState;
+    shareState: OpState;
 }
 
 
 class PublicCauseWidget extends React.Component<PublicCauseWidgetProps, PublicCauseWidgetState> {
     private static readonly _initialState: PublicCauseWidgetState = {
-        shareIsLoading: false,
-        shareIsReady: false,
-        shareIsFailed: false
+        donationAmount: new UserInput<number, number>(10, 10),
+        donationState: OpState.Init,
+        shareState: OpState.Init
     };
+
+    private readonly _donationAmountMaster: UserInputMaster<number, number>;
     
     constructor(props: PublicCauseWidgetProps, context: any) {
         super(props, context);
         this.state = (Object as any).assign({}, PublicCauseWidget._initialState);
+	this._donationAmountMaster = new UserInputMaster<number, number>(new r.PositiveIntegerMarshaller());
     }
     
     render() {
+        const allValid = !this.state.donationAmount.isInvalid();
+        
+        let donationResult = <span></span>;
+        switch (this.state.donationState) {
+        case OpState.Loading:
+            donationResult = <span>Donating</span>;
+            break;
+        case OpState.Ready:
+            donationResult = <span>Ready</span>;
+            break;
+        case OpState.Failed:
+            donationResult = <span>Failed</span>;
+            break;
+        } 
+        
         let shareResult = <span></span>;
-        if (this.state.shareIsLoading) {
+        switch (this.state.donationState) {
+        case OpState.Loading:
             shareResult = <span>Sharing</span>;
-        } else if (this.state.shareIsReady) {
+            break;
+        case OpState.Ready:
             shareResult = <span>Ready</span>;
-        } else if (this.state.shareIsFailed) {
+            break;
+        case OpState.Failed:
             shareResult = <span>Failed</span>;
+            break;
         }
         
 	return (
@@ -433,16 +455,45 @@ class PublicCauseWidget extends React.Component<PublicCauseWidgetProps, PublicCa
 		<p>{this.props.cause.goal.amount} - {this.props.cause.goal.currency.toString()}</p>
 		<p>{this.props.cause.deadline.toString()}</p>
                 <ImageGallery pictureSet={this.props.cause.pictureSet} />
+                <button type="button" onClick={_ => this._handleSetDonationAmount(10)}>10</button>
+                <button type="button" onClick={_ => this._handleSetDonationAmount(25)}>25</button>
+                <button type="button" onClick={_ => this._handleSetDonationAmount(50)}>50</button>
+                <span>{this.state.donationAmount.getValue()} - {this.props.cause.goal.currency.toString()}</span>
+                <button disabled={!allValid} type="button" onClick={this._handleDonate.bind(this)}>Donate</button>
+                {donationResult}
                 <button type="button" onClick={this._handleShare.bind(this)}>Share</button>
                 {shareResult}
 	    </div>
 	);
     }
 
+    private _handleSetDonationAmount(amount: number) {
+        this.setState({donationAmount: this._donationAmountMaster.transform(amount, this.state.donationAmount.getValue())});
+    }
+
+    private async _handleDonate() {
+        this.setState({donationState: OpState.Loading});
+        
+        try {
+            const currencyAmount = new CurrencyAmount();
+            currencyAmount.amount = this.state.donationAmount.getValue();
+            currencyAmount.currency = this.props.cause.goal.currency;
+            
+            await corePublicClient.createDonation(accessToken, this.props.cause.id, currencyAmount);
+            this.setState({donationState: OpState.Ready});
+        } catch (e) {
+            if (isLocal(config.ENV)) {
+                console.log(e);
+            }
+            
+            this.setState({donationState: OpState.Failed});
+        }
+    }
+
     private _handleShare() {
         const href = `${window.location.protocol}//${window.location.hostname}:${window.location.port}${_causeLink(this.props.cause)}`;
 
-        this.setState({shareIsLoading: true, shareIsReady: false, shareIsFailed: false});
+        this.setState({shareState: OpState.Loading});
         
         FB.ui({
             method: 'share',
@@ -451,30 +502,29 @@ class PublicCauseWidget extends React.Component<PublicCauseWidgetProps, PublicCa
             console.log(response);
             if (typeof response === 'undefined') {
                 // User closed dialog without sharing.
-                this.setState({shareIsLoading: false, shareIsReady: false, shareIsFailed: true});
+                this.setState({shareState: OpState.Failed});
                 return;
             }
 
             if (response.hasOwnProperty('error_message')) {
-                this.setState({shareIsLoading: false, shareIsReady: false, shareIsFailed: true});
+                this.setState({shareState: OpState.Failed});
                 return;
             }
 
             if (!response.hasOwnProperty('post_id')) {
-                this.setState({shareIsLoading: false, shareIsReady: false, shareIsFailed: true});
+                this.setState({shareState: OpState.Failed});
                 return;
             }
 
             try {
                 await corePublicClient.createShare(accessToken, this.props.cause.id, response.post_id as string);
-                this.setState({shareIsLoading: false, shareIsReady: true, shareIsFailed: false});
+                this.setState({shareState: OpState.Ready});
             } catch (e) {
-                console.log(e);
                 if (isLocal(config.ENV)) {
                     console.log(e);
                 }
                 
-                this.setState({shareIsLoading: false, shareIsReady: false, shareIsFailed: true});
+                this.setState({shareState: OpState.Failed});
             }
         });
     }
