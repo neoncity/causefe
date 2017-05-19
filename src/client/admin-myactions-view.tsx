08cc745b5a6b586ade1687160cb298ae4645afd2
Horia Coman <horia141@gmail.com>
2017-05-19 17:55:42 +0300

    Extract AdminMyActionsView

---
+++ src/client/admin-myactions-view.tsx
@@ -0,0 +1,95 @@
+import * as React from 'react'
+import { connect } from 'react-redux'
+
+import { UserActionsOverview } from '@neoncity/core-sdk-js'
+import { isLocal } from '@neoncity/common-js'
+
+import { accessToken } from './access-token'
+import * as config from './config'
+import { DonationForUserWidget } from './donation-for-user-widget'
+import { corePrivateClient } from './services'
+import { ShareForUserWidget } from './share-for-user-widget'
+import { AdminMyActionsState, OpState, StatePart } from './store'
+
+
+interface AdminMyActionsProps {
+    isLoading: boolean;
+    isReady: boolean;
+    isFailed: boolean;
+    userActionsOverview: UserActionsOverview|null;
+    errorMessage: string|null;
+    onUserActionsOverviewLoading: () => void;
+    onUserActionsOverviewReady: (userActionsOverview: UserActionsOverview) => void;
+    onUserActionsOverviewFailed: (errorMessage: string) => void;
+}
+
+
+class _AdminMyActionsView extends React.Component<AdminMyActionsProps, undefined> {
+    async componentDidMount() {
+	this.props.onUserActionsOverviewLoading();
+
+	try {
+	    const userActionsOverview = await corePrivateClient.getActionsOverview(accessToken);
+	    this.props.onUserActionsOverviewReady(userActionsOverview);
+	} catch (e) {
+            if (isLocal(config.ENV)) {
+                console.log(e);
+            }
+            
+	    this.props.onUserActionsOverviewFailed('Could not load user actions overview');
+	}
+    }
+    
+    render() {
+	if (this.props.isLoading) {
+	    return (<div>Loading ...</div>);
+	} else if (this.props.isFailed) {
+	    return (<div>Failed {this.props.errorMessage}</div>);
+	} else {
+	    const donationWidgets = (this.props.userActionsOverview as UserActionsOverview)
+		  .donations
+		  .slice(0) // clone
+		  .sort((a, b) => b.timeCreated.getTime() - a.timeCreated.getTime())
+		  .map((d) => <DonationForUserWidget key={d.id} donationForUser={d} />);
+	    const shareWidgets = (this.props.userActionsOverview as UserActionsOverview)
+		  .shares
+		  .slice(0) // clone
+		  .sort((a, b) => b.timeCreated.getTime() - a.timeCreated.getTime())
+		  .map((d) => <ShareForUserWidget key={d.id} shareForUser={d} />);	    
+
+	    return (
+                <div>
+		    <h6>Donations</h6>
+		    {donationWidgets}
+		    <h6>Shares</h6>
+		    {shareWidgets}
+		</div>
+	    );
+	}
+    }
+}
+
+
+function stateToProps(state: any) {
+    return {
+	isLoading: state.adminMyActions.type == OpState.Init || state.adminMyActions.type == OpState.Loading,
+	isReady: state.adminMyActions.type == OpState.Ready,
+	isFailed: state.adminMyActions.type == OpState.Failed,
+	userActionsOverview: state.adminMyActions.type == OpState.Ready ? state.adminMyActions.userActionsOverview : null,
+	errorMessage: state.adminMyActions.type == OpState.Failed ? state.adminMyActions.errorMessage : null
+    };
+}
+
+
+function dispatchToProps(dispatch: (newState: AdminMyActionsState) => void) {
+    return {
+	onUserActionsOverviewLoading: () => dispatch({part: StatePart.AdminMyActions, type: OpState.Loading}),
+	onUserActionsOverviewReady: (userActionsOverview: UserActionsOverview) => dispatch({part: StatePart.AdminMyActions, type: OpState.Ready, userActionsOverview: userActionsOverview}),
+	onUserActionsOverviewFailed: (errorMessage: string) => dispatch({part: StatePart.AdminMyActions, type: OpState.Failed, errorMessage: errorMessage})
+    };
+}
+
+
+export const AdminMyActionsView = connect(
+    stateToProps,
+    dispatchToProps)(_AdminMyActionsView);
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -15,25 +15,23 @@ import { BankInfo,
 	 CurrencyAmount,
 	 PictureSet,
 	 PrivateCause,
-	 UserActionsOverview,
 	 TitleMarshaller,
 	 DescriptionMarshaller} from '@neoncity/core-sdk-js'
 import { User } from '@neoncity/identity-sdk-js'
 
 import { accessToken } from './access-token'
 import { AdminAccountView } from './admin-account-view'
+import { AdminMyActionsView } from './admin-myactions-view'
 import { AppFrame } from './app-frame'
 import { BankInfoWidget } from './bank-info-widget'
 import { CauseView } from './cause-view'
 import * as config from './config'
-import { DonationForUserWidget } from './donation-for-user-widget'
 import { HomeView } from './home-view'
 import { IdentityFrame } from './identity-frame'
 import { ImageGalleryEditorWidget } from './image-gallery-editor-widget'
 import './index.less'
-import { ShareForUserWidget } from './share-for-user-widget'
 import { corePrivateClient, fileStorageService } from './services'
-import { AdminCauseAnalyticsState, AdminMyActionsState, AdminMyCauseState, OpState, StatePart, store } from './store'
+import { AdminCauseAnalyticsState, AdminMyCauseState, OpState, StatePart, store } from './store'
 import { UserInput, UserInputMaster } from './user-input'
 
 // Old style imports.
@@ -561,89 +559,6 @@ function adminCauseAnalyticsMapDispatchToProps(dispatch: (newState: AdminCauseAn
 const AdminCauseAnalyticsView = connect(
     adminCauseAnalyticsMapStateToProps,
     adminCauseAnalyticsMapDispatchToProps)(_AdminCauseAnalyticsView);
-
-
-interface AdminMyActionsProps {
-    isLoading: boolean;
-    isReady: boolean;
-    isFailed: boolean;
-    userActionsOverview: UserActionsOverview|null;
-    errorMessage: string|null;
-    onUserActionsOverviewLoading: () => void;
-    onUserActionsOverviewReady: (userActionsOverview: UserActionsOverview) => void;
-    onUserActionsOverviewFailed: (errorMessage: string) => void;
-}
-
-
-class _AdminMyActionsView extends React.Component<AdminMyActionsProps, undefined> {
-    async componentDidMount() {
-	this.props.onUserActionsOverviewLoading();
-
-	try {
-	    const userActionsOverview = await corePrivateClient.getActionsOverview(accessToken);
-	    this.props.onUserActionsOverviewReady(userActionsOverview);
-	} catch (e) {
-            if (isLocal(config.ENV)) {
-                console.log(e);
-            }
-            
-	    this.props.onUserActionsOverviewFailed('Could not load user actions overview');
-	}
-    }
-    
-    render() {
-	if (this.props.isLoading) {
-	    return (<div>Loading ...</div>);
-	} else if (this.props.isFailed) {
-	    return (<div>Failed {this.props.errorMessage}</div>);
-	} else {
-	    const donationWidgets = (this.props.userActionsOverview as UserActionsOverview)
-		  .donations
-		  .slice(0) // clone
-		  .sort((a, b) => b.timeCreated.getTime() - a.timeCreated.getTime())
-		  .map((d) => <DonationForUserWidget key={d.id} donationForUser={d} />);
-	    const shareWidgets = (this.props.userActionsOverview as UserActionsOverview)
-		  .shares
-		  .slice(0) // clone
-		  .sort((a, b) => b.timeCreated.getTime() - a.timeCreated.getTime())
-		  .map((d) => <ShareForUserWidget key={d.id} shareForUser={d} />);	    
-
-	    return (
-                <div>
-		    <h6>Donations</h6>
-		    {donationWidgets}
-		    <h6>Shares</h6>
-		    {shareWidgets}
-		</div>
-	    );
-	}
-    }
-}
-
-
-function adminMyActionsMapStateToProps(state: any) {
-    return {
-	isLoading: state.adminMyActions.type == OpState.Init || state.adminMyActions.type == OpState.Loading,
-	isReady: state.adminMyActions.type == OpState.Ready,
-	isFailed: state.adminMyActions.type == OpState.Failed,
-	userActionsOverview: state.adminMyActions.type == OpState.Ready ? state.adminMyActions.userActionsOverview : null,
-	errorMessage: state.adminMyActions.type == OpState.Failed ? state.adminMyActions.errorMessage : null
-    };
-}
-
-
-function adminMyActionsMapDispatchToProps(dispatch: (newState: AdminMyActionsState) => void) {
-    return {
-	onUserActionsOverviewLoading: () => dispatch({part: StatePart.AdminMyActions, type: OpState.Loading}),
-	onUserActionsOverviewReady: (userActionsOverview: UserActionsOverview) => dispatch({part: StatePart.AdminMyActions, type: OpState.Ready, userActionsOverview: userActionsOverview}),
-	onUserActionsOverviewFailed: (errorMessage: string) => dispatch({part: StatePart.AdminMyActions, type: OpState.Failed, errorMessage: errorMessage})
-    };
-}
-
-
-const AdminMyActionsView = connect(
-    adminMyActionsMapStateToProps,
-    adminMyActionsMapDispatchToProps)(_AdminMyActionsView);
 
 
 
