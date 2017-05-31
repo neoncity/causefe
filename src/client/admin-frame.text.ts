5af34303c304048cf98381f21ecb7d2f6956e660
Horia Coman <horia141@gmail.com>
2017-05-31 23:25:33 +0300

    Add text file for admin frame

---
+++ src/client/admin-frame.text.ts
@@ -0,0 +1,26 @@
+import { Message } from './messages'
+
+export const viewTitle: Message = {
+    en: "This is the admin view",
+    ro: "Acesta este panoul de administrație"
+};
+
+export const myCause: Message = {
+    en: "My cause",
+    ro: "Cauza mea"
+};
+
+export const causeAnalytics: Message = {
+    en: "Cause analytics",
+    ro: "Statistici"
+};
+
+export const myActions: Message = {
+    en: "My actions",
+    ro: "Faptele mele"
+};
+
+export const account: Message = {
+    en: "Account",
+    ro: "Cont"
+};
--- src/client/admin-frame.tsx
+++ src/client/admin-frame.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
 import { Link } from 'react-router'
 
+import { LANG } from './from-server'
+
+import * as text from './admin-frame.text'
+
 
 interface Props {
     children: React.ReactNode;
@@ -11,11 +15,11 @@ export class AdminFrame extends React.Component<Props, undefined> {
     render() {
         return (
             <div>
-                <div>This is the admin view</div>
-                <Link to="/admin/my-cause">My Cause</Link>
-                <Link to="/admin/cause-analytics">Cause Analytics</Link>
-                <Link to="/admin/my-actions">My Actions</Link>
-                <Link to="/admin/account">Account</Link>
+                <div>{text.viewTitle[LANG]}</div>
+                <Link to="/admin/my-cause">{text.myCause[LANG]}</Link>
+                <Link to="/admin/cause-analytics">{text.causeAnalytics[LANG]}</Link>
+                <Link to="/admin/my-actions">{text.myActions[LANG]}</Link>
+                <Link to="/admin/account">{text.account[LANG]}</Link>
                 {this.props.children}
             </div>
         );
