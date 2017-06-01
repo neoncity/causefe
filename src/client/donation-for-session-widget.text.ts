063cd3e62bd9d925eb0cdf8d26c9586c27387995
Horia Coman <horia141@gmail.com>
2017-06-01 18:35:32 +0300

    Add text for donation for session widget

---
+++ src/client/donation-for-session-widget.text.ts
@@ -0,0 +1,13 @@
+import { CurrencyAmount } from '@neoncity/core-sdk-js'
+
+import { Message, MessageWith3Arg } from './messages'
+
+export const donated: MessageWith3Arg = {
+    en: (amount: CurrencyAmount, causeTitle: string, timeCreated: Date) => `Donated ${amount.amount} ${amount.currency.toString()} to ${causeTitle} on ${timeCreated}`,
+    ro: (amount: CurrencyAmount, causeTitle: string, timeCreated: Date) => `Am donat ${amount.amount} ${amount.currency.toString()} pentru ${causeTitle} la ${timeCreated}`
+};
+
+export const details: Message = {
+    en: "Details",
+    ro: "Detalii"
+};
--- src/client/donation-for-session-widget.tsx
+++ src/client/donation-for-session-widget.tsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router'
 
 import { DonationForSession } from '@neoncity/core-sdk-js'
 
+import { LANG } from './from-server'
 import { causeLink } from './utils'
+
+import * as text from './donation-for-session-widget.text'
 
 
 interface Props {
@@ -16,7 +19,7 @@ export class DonationForSessionWidget extends React.Component<Props, null> {
 	const donation = this.props.donationForSession;
 	const cause = this.props.donationForSession.forCause;
 	const timeCreated = donation.timeCreated.toString();
-	
-	return <p>To <Link to={causeLink(cause)}>{cause.title}</Link> donated {donation.amount.amount} {donation.amount.currency.toString()} on {timeCreated}</p>;
+
+	return <p>{text.donated[LANG](donation.amount, cause.title, timeCreated)} <Link to={causeLink(cause)}>text.details[LANG]</Link></p>
     }
 }
