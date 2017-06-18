d8f7b232f8ced8bd37d832f599b4a05d2e325444
Horia Coman <horia141@gmail.com>
2017-06-18 12:50:10 +0300

    Extract a manifest chunk and a commons vendor chunk and load them separately

--- webpack.config.js
+++ webpack.config.js
@@ -110,7 +110,17 @@ module.exports = {
 	    {from: './src/shared/static/sitemap.xml'}
 	]),
 	new ExtractTextPlugin('client.css'),
-        new webpack.NoEmitOnErrorsPlugin()
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module) {
+                // this assumes your vendor imports exist in the node_modules directory
+                return module.context && module.context.indexOf('node_modules') !== -1;
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest'
+        })
     ].concat(process.env.ENV !== 'LOCAL' ? prodPlugins : []),
     resolve: {
         extensions: ['.js', '.ts', '.tsx', '.css', '.less'],
