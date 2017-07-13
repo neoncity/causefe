0e22eef7cc16010d24c6ddf4ab58d73f56383790
Horia Coman <horia141@gmail.com>
2017-07-13 20:25:13 +0300

    Fixed the account view styles.
    But right now there's just nothing to control, so we don't.

--- src/shared/admin-account-view.text.ts
+++ src/shared/admin-account-view.text.ts
@@ -7,6 +7,6 @@ export const pageTitle: Message = {
 };
 
 export const viewTitle: Message = {
-    en: "This is the account section",
-    ro: "Aceasta este secțiunea de configrare a contului"
+    en: "This is the account section. There's nothing here yet. We're working hard to add controls for removing your account and its associated data and changing the language.",
+    ro: "Aceasta este secțiunea de configrare a contului. Încă nu există nimic aici. Muncim din greu pentru a adaugă posibilitatea de a șterge contul și de a schimba limba."
 }
