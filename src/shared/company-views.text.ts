157997685f4c612e0c641f330e786a6385a8e6b2
Horia Coman <horia141@gmail.com>
2017-08-12 20:15:22 +0300

    Generate head metadata for company pages.

--- src/shared/company-views.text.ts
+++ src/shared/company-views.text.ts
@@ -1,6 +1,17 @@
 import { MessageWith0Arg } from './messages'
 
 
+export const aboutPageTitle: MessageWith0Arg = {
+    en: "About company",
+    ro: "Despre companie"
+};
+
+export const aboutPageDescription: MessageWith0Arg = {
+    en: "About company",
+    ro: "Despre companie"
+};
+
+
 export const about: MessageWith0Arg = {
     en: `
 This is the _about_ page
@@ -8,6 +19,16 @@ This is the _about_ page
     ro: `
 Aceasta este pagina despre _companie_
     `
+};
+
+export const termsPageTitle: MessageWith0Arg = {
+    en: "Terms and conditions",
+    ro: "Condiţii de utilizare"
+};
+
+export const termsPageDescription: MessageWith0Arg = {
+    en: "Terms and conditions",
+    ro: "Condiţii de utilizare"
 };
 
 export const terms: MessageWith0Arg = {
@@ -19,6 +40,16 @@ Aceasta este pagina _condiţii de utilizare_
     `
 };
 
+export const privacyPageTitle: MessageWith0Arg = {
+    en: "Privacy policy",
+    ro: "Politica de confidenţialitate"
+};
+
+export const privacyPageDescription: MessageWith0Arg = {
+    en: "Privacy policy",
+    ro: "Politica de confidenţialitate"
+};
+
 export const privacy: MessageWith0Arg = {
     en: `
 This is the _privacy_ page
@@ -26,6 +57,16 @@ This is the _privacy_ page
     ro: `
 Aceasta este pagina despre _confidenţialitate_
     `
+};
+
+export const cookiesPageTitle: MessageWith0Arg = {
+    en: "Cookies policy",
+    ro: "Politica de module cookies"
+};
+
+export const cookiesPageDescription: MessageWith0Arg = {
+    en: "Cookies policy",
+    ro: "Politica de module cookies"
 };
 
 export const cookies: MessageWith0Arg = {
